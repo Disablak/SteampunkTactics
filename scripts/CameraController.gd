5dd008e4e3f0f8d5105683b5086a03678d7981a0
disablak <disablaksteam@gmail.com>
2022-12-20 17:26:15 +0200

    added bound

--- scripts/CameraController.gd
+++ scripts/CameraController.gd
@@ -1,27 +1,13 @@
 extends Node2D
 
-@export var drag_sensitive = 0.02
-@export var camera_offset = Vector2.ZERO
-@export var focus_time = 0.2
 
-var rot = Quaternion(Vector3.UP, -45).normalized()
+@export var focus_time = 0.2
 
 var tween_move: Tween
 
 
-func _init():
-	#GlobalBus.connect(GlobalBus.on_setted_unit_control_name,Callable(self,"focus_camera"))
-	pass
-
-
-func _on_input_system_on_drag(dir) -> void:
-	global_transform.origin += rot * (Vector3(dir.x, 0.0, dir.y) * drag_sensitive)
-
-
 func focus_camera(target: Node2D, instantly: bool = false):
-	pass
-	var target_pos: Vector2 = target.global_transform.origin + camera_offset
-	target_pos.y = global_transform.origin.y
+	var target_pos: Vector2 = target.position
 
 	if instantly:
 		position = target_pos
@@ -33,5 +19,5 @@ func focus_camera(target: Node2D, instantly: bool = false):
 		self,
 		"position",
 		target_pos,
-		focus_time if not instantly else 0.0
+		focus_time
 	)
--- scripts/InputSystem.gd
+++ scripts/InputSystem.gd
@@ -6,21 +6,34 @@ signal on_mouse_click(cell_info: CellInfo)
 signal on_drag(dir)
 signal on_pressed_esc()
 
+@export var drag_sensitive = 0.5
+
 @onready var camera_controller := get_node("CameraController")
 @onready var camera := get_node("CameraController/Camera2d") as Camera2D
+@onready var camera_bounds: Node2D = get_node("CameraBounds") as Node2D
+
 @onready var cell_info: = CellInfo.new()
 
-const ray_length = 1000
+const TIME_CLICK_MS = 150
 
 var dragging: bool
 var drag_pos: Vector2
 var prev_drag_pos: Vector2
 var prev_hover_pos: Vector3
 
+var was_mouse_btn_pressed: bool = false
+var time_pressed: int
+
+var camera_bound_rect: Rect2
+
+
+func _ready() -> void:
+	camera_bound_rect = Rect2(camera_bounds.position, camera_bounds.scale * Globals.CELL_SIZE)
+
 
 func _input(event: InputEvent) -> void:
-#	if _draging(event):
-#		return
+	if _draging(event):
+		return
 
 	_mouse_hover(event)
 	_mouse_click(event)
@@ -36,6 +49,8 @@ func _draging(event: InputEvent) -> bool:
 	elif event is InputEventMouseMotion and dragging:
 		drag_pos = (prev_drag_pos - event.position)
 		prev_drag_pos = event.position
+
+		_drag(drag_pos)
 		emit_signal("on_drag", drag_pos)
 		return true
 
@@ -43,10 +58,23 @@ func _draging(event: InputEvent) -> bool:
 
 
 func _mouse_click(event: InputEvent):
-	if event is InputEventMouseButton and event.pressed:
-		cell_info.reset()
-		cell_info.cell_pos = formatted_position(event.position)
-		on_mouse_click.emit(cell_info)
+	if event is InputEventMouseButton:
+		if was_mouse_btn_pressed && not event.pressed:
+			var time_released = Time.get_ticks_msec()
+			if time_released - time_pressed > TIME_CLICK_MS:
+				return
+
+			was_mouse_btn_pressed = false
+
+			cell_info.reset()
+			cell_info.cell_pos = formatted_position(event.position)
+			on_mouse_click.emit(cell_info)
+			return
+
+		if event.pressed:
+			was_mouse_btn_pressed = true
+			time_pressed = Time.get_ticks_msec()
+			return
 
 
 func _mouse_hover(event: InputEvent):
@@ -61,6 +89,19 @@ func _mouse_hover(event: InputEvent):
 func _click_escape(event: InputEvent):
 	if event.is_action_pressed("ui_cancel"):
 		on_pressed_esc.emit()
+
+
+func _drag(dir: Vector2) -> void:
+	var half_bound_size := camera_bound_rect.size / 2
+	var half_camera_size := get_viewport_rect().size / camera.zoom / 2
+
+	var min_x = -half_bound_size.x + camera_bound_rect.position.x + half_camera_size.x
+	var max_x =  half_bound_size.x + camera_bound_rect.position.x - half_camera_size.x
+	var min_y = -half_bound_size.y + camera_bound_rect.position.y + half_camera_size.y
+	var max_y =  half_bound_size.y + camera_bound_rect.position.y - half_camera_size.y
+
+	var new_pos = camera_controller.position + dir * drag_sensitive
+	camera_controller.position = Vector2(clampf(new_pos.x, min_x, max_x), clampf(new_pos.y, min_y, max_y))
 
 
 func formatted_position(position: Vector2) -> Vector2:
