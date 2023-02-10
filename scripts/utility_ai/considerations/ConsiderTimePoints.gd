d529b6096ae151778e5ea5881410600fd0b6e59f
disablak <disablaksteam@gmail.com>
2023-02-10 13:59:15 +0200

    removed useles

--- scripts/utility_ai/considerations/ConsiderTimePoints.gd
+++
@@ -1,8 +0,0 @@
-class_name ConsiderTimePoints
-extends Consideration
-
-
-func calc_score() -> float:
-	var result = float(TurnManager.cur_time_points) / float(TurnManager.max_time_points)
-	print("time points {0}".format([result]))
-	return result
