00d89fb7e6e64597b46663b0a4d8eaab00192f81
disablak <disablaksteam@gmail.com>
2023-07-09 16:38:22 +0300

    up

--- scripts/ui/UnitSetupUI.gd
+++ scripts/ui/UnitSetupUI.gd
@@ -8,6 +8,7 @@ extends Control
 @onready var container_available_equip: Control = %VBoxContainerAvailableEquip
 @onready var button_ready: MyButton = %ButtonReady
 @onready var unit_tabs: TabBar = %TabBar
+@onready var label_unit_info: Label = %LabelUnitInfo
 
 
 var game_progress: GameProgress
@@ -17,6 +18,8 @@ var callback_click_ready: Callable
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	unit_tabs.tab_changed.connect(_on_change_tab)
+
 	for ability in GlobalDB.weapon_settings:
 		var new_button: MyButton = button_scene.instantiate() as MyButton
 		new_button.text = ability.ability_name
@@ -45,6 +48,14 @@ func move_to_available_equip(btn):
 	container_unit_equip.remove_child(btn)
 	container_available_equip.add_child(btn)
 	btn.click_action = func(): return move_to_unit_equip(btn)
+
+
+func _on_change_tab(id):
+	show_unit_info(game_progress.units_setting[id])
+
+
+func show_unit_info(unit_setting: UnitSetting):
+	label_unit_info.text = "Health {0}\nWalk speed {1}\nInitiative {2}".format([unit_setting.max_health, unit_setting.walk_speed, unit_setting.initiative])
 
 
 func _on_button_ready_button_down() -> void:
